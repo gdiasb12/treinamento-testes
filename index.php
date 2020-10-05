4a10b09f65d7c976f03047afa33f4e74d240334d
Gabriel Dias <gdiasb12@gmail.com>
2020-10-05 12:11:46 -0300

    First exercise done

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <?php
-ini_set('display_errors', 1);
-
 require_once __DIR__. '/vendor/autoload.php';
 
 use App\Hello;
-use App\Multiple;
 
-// $hello = new Hello();
+$hello = new Hello();
 
-// echo $hello->sayHelloWorld();
-
-$multiple = new Multiple();
-
-echo $multiple->calculateMultiples(1);
+echo $hello->sayHelloWorld();

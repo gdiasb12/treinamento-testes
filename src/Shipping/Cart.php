255a39a3cee375f7ee652069147f6f0c00d75f1d
Gabriel Dias <gdiasb12@gmail.com>
2020-10-13 12:14:03 -0300

    Fourth exercise in progress

--- src/Shipping/Cart.php
+++ src/Shipping/Cart.php
@@ -9,28 +9,19 @@ use App\Shipping\Product;
 class Cart
 {
     private User $user;
-    // /** @var array<Product> $itemCart a list of products with the amount */
-<<<<<<< HEAD
+    
     private array $itemsCart = [];
-=======
-    private array $itensCart = [];
->>>>>>> 8f8aa26f3a0befacf6685a96f9d25610ba36947c
 
     public function __construct(User $user)
     {
         $this->user = $user;
     }
 
-<<<<<<< HEAD
     public function getUser(): User
-=======
-    public function getUser()
->>>>>>> 8f8aa26f3a0befacf6685a96f9d25610ba36947c
     {
         return $this->user;
     }
 
-<<<<<<< HEAD
     public function setItemCart(Product $product, int $amount = 1): void
     {
         for ($count = 1; $count <= $amount; $count++) {
@@ -46,47 +37,20 @@ class Cart
     public function countitemsCart()
     {
         return count($this->itemsCart);
-=======
-    public function setItemCart(Product $product, int $amount = 1)
-    {
-        for ($count = 1; $count <= $amount; $count++) {
-            array_push($this->itensCart, $product);
-        }
-    }
-
-    public function getItensCart()
-    {
-        return $this->itensCart;
-    }
-
-    public function countItensCart()
-    {
-        return count($this->itensCart);
->>>>>>> 8f8aa26f3a0befacf6685a96f9d25610ba36947c
     }
 
     public function removeItemCart($productName, $amount = 1): void
     {
-<<<<<<< HEAD
         $itemsCart = $this->itemsCart;
 
         for ($count = 1; $count <= $amount; $count++) {
             foreach ($itemsCart as $key => $item) {
                 if ($productName == $item->getName()) {
                     unset($itemsCart[$key]);
-=======
-        $itensCart = $this->itensCart;
-
-        for ($count = 1; $count <= $amount; $count++) {
-            foreach ($itensCart as $key => $item) {
-                if ($productName == $item->getName()) {
-                    unset($itensCart[$key]);
->>>>>>> 8f8aa26f3a0befacf6685a96f9d25610ba36947c
                     break;
                 }
             }
         }
-<<<<<<< HEAD
 
         $this->itemsCart = $itemsCart;
     }
@@ -106,9 +70,5 @@ class Cart
         }
 
         return number_format($totalPrice, 2, '.', '');
-=======
-        
-        $this->itensCart = $itensCart;
->>>>>>> 8f8aa26f3a0befacf6685a96f9d25610ba36947c
     }
 }

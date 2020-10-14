20b0329988ddc75c68c9d0f55fb6f7549dd7aa0b
Gabriel Dias <gdiasb12@gmail.com>
2020-10-14 17:51:46 -0300

    Refactoring

---
+++ src/Shipping/Model/Cart.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Shipping\Model;
+
+use App\Shipping\Model\User;
+use App\Shipping\Model\Product;
+
+class Cart
+{
+    private User $user;
+    
+    private array $itemsCart = [];
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setItemCart(Product $product, int $amount = 1): void
+    {
+        for ($count = 1; $count <= $amount; $count++) {
+            array_push($this->itemsCart, $product);
+        }
+    }
+
+    public function getItemsCart(): array
+    {
+        return $this->itemsCart;
+    }
+
+    public function countitemsCart()
+    {
+        return count($this->itemsCart);
+    }
+
+    public function removeItemCart($productName, $amount = 1): void
+    {
+        $itemsCart = $this->itemsCart;
+
+        for ($count = 1; $count <= $amount; $count++) {
+            foreach ($itemsCart as $key => $item) {
+                if ($productName == $item->getName()) {
+                    unset($itemsCart[$key]);
+                    break;
+                }
+            }
+        }
+
+        $this->itemsCart = $itemsCart;
+    }
+
+    public function removeAllItems(): void
+    {
+        $this->itemsCart = [];
+    }
+
+    public function calculateTotalPrice(): float
+    {
+        $itemsCart = $this->itemsCart;
+        $totalPrice = 0;
+
+        foreach ($itemsCart as $item) {
+            $totalPrice += $item->getPrice();
+        }
+
+        return $totalPrice;
+    }
+}
---
+++ tests/Shipping/Model/UserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Shipping;
+
+use App\Shipping\Model\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    private User $user;
+    private string $name;
+    private string $address;
+
+    protected function setUp(): void
+    {
+        $this->name = "Gabriel";
+        $this->address = "15600000";
+
+        $this->user = new User($this->name, $this->address);
+    }
+
+    public function testIfAClassCanBeInstantiated(): void
+    {
+        $this->assertInstanceOf(User::class, $this->user);
+    }
+
+    public function testReturnsAnUserObject(): void
+    {
+        $this->assertEquals($this->name, $this->user->getName());
+        $this->assertEquals($this->address, $this->user->getAddress());
+    }
+}
